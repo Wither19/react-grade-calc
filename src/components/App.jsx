792a414941bcb8b33d865d96dc77bbac54c4e9ad
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-03-03 18:44:53 +0000

    c

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -70,6 +70,8 @@ function App() {
 		94.48,
 	]);
 
+	const [showNoGradeClasses, setShowNoGradeClasses] = useState(true);
+
 	const average = (list, decimal) => {
 		parseFloat(
 			(list.reduce((a, b) => a + b) / list.length).toFixed(
@@ -78,39 +80,46 @@ function App() {
 		);
 	};
 
-	const [gradeAverage, setAverage] = useState(average(grades, 2));
+	const [gradeAverage, setAverage] = useState(average(_.compact(grades), 2));
 
 	return (
 		<>
+			<label for="no-grade-classes">Show Classes Without Grades</label>
+			<input
+				id="no-grade-classes"
+				type="checkbox"
+				checked={showNoGradeClasses}
+				onChange={() => setShowNoGradeClasses((prev) => !prev)}
+			/>
 			<table>
 				<tr>
 					<th>Class Name</th>
 					<th>Grade</th>
 				</tr>
 				{classList.map((course, index) => (
-					<tr>
-						<td>{course.name}</td>
-						<td
-							onClick={() => {
-								const newGrade = prompt("Enter a new grade for " + course.name);
-								if (newGrade !== null) {
-									const courseIndex = classList.findIndex(
-										(c) => c.name === course.name
+					<>
+						<tr>
+							<td>{course.name}</td>
+							<td
+								onClick={() => {
+									const newGrade = prompt(
+										"Enter a new grade for " + course.name
 									);
-									const updatedGrades = [...grades];
-									updatedGrades[courseIndex] = parseFloat(newGrade);
-									setGrades(updatedGrades);
-								}
-							}}>
-							{!!grades[index] ? `${grades[index]}%` : "N/A"}
-						</td>
-					</tr>
+									if (newGrade !== null) {
+										const courseIndex = classList.findIndex(
+											(c) => c.name === course.name
+										);
+										const updatedGrades = [...grades];
+										updatedGrades[courseIndex] = parseFloat(newGrade);
+										setGrades(updatedGrades);
+									}
+								}}>
+								{!!grades[index] ? `${grades[index]}%` : "N/A"}
+							</td>
+						</tr>
+					</>
 				))}
 			</table>
-			<br />
-			<br />
-			<br />
-			<b>{average}%</b>
 		</>
 	);
 }
