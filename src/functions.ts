23932f727ff159599dcca0ffe664b2b58c5f74b8
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-03-25 13:52:11 +0000

    c

--- src/functions.ts
+++ src/functions.ts
@@ -7,8 +7,13 @@ export const average = (list: Array<any>, decimal?: number): number => {
 };
 
 // Pass previous state reference to the array argument
-export const sortListState = (list: Array<any>, sortType: string, ascending: boolean) {
-		let newList: Array<any> = _.sortBy(list, [sortType]);
-		newList = ascending ? newList : _.reverse(newList);
-		return newList;
-}
+export const sortListState = (
+	list: Array<any>,
+	sortType: string,
+	ascending: boolean
+): Array<any> => {
+	let newList: Array<any> = ascending
+		? _.sortBy(list, [sortType])
+		: _.reverse(_.sortBy(list, [sortType]));
+	return newList;
+};
