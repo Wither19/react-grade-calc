0ab14bd794401caeaa005181b2868cd736cc5954
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-03-25 12:24:10 +0000

    c

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,49 +12,61 @@ type BlockGrade = {
 	grade: number | null;
 };
 
+type InputEditable = {
+	isEditable: boolean;
+};
+
 function App() {
-	var classList: Array<BlockGrade> = [
+	var classList: Array<BlockGrade & InputEditable> = [
 		{
 			block: 1,
 			name: "Marine Science",
 			grade: 90.16,
+			isEditable: false,
 		},
 		{
 			block: 2,
 			name: "English III",
 			grade: 97.58,
+			isEditable: false,
 		},
 		{
 			block: 3,
 			name: "Apologetics",
 			grade: 91.01,
+			isEditable: false,
 		},
 		{
 			block: 4,
 			name: "Spanish II",
 			honors: true,
 			grade: 84.87,
+			isEditable: false,
 		},
 		{
 			block: 5,
 			name: "Geometry",
 			grade: 90.72,
+			isEditable: false,
 		},
 		{
 			block: 6,
 			name: "Study Hall",
 			grade: null,
+			isEditable: false,
 		},
 		{
 			block: 7,
 			name: "Computer Science Essentials",
 			honors: true,
 			grade: 90.66,
+			isEditable: false,
 		},
 		{
 			block: 8,
 			name: "US History",
 			grade: 94.48,
+			isEditable: false,
 		},
 	];
 
@@ -76,7 +88,15 @@ function App() {
 		}
 	}, [omitNoGrade]);
 
-	const changeToInput = (e) => {};
+	const changeToInput = (index: number) => {
+		classList[index].isEditable = true;
+		setClasses(classList);
+	};
+
+	const changeToTableData = (index: number) => {
+		classList[index].isEditable = false;
+		setClasses(classList);
+	};
 
 	return (
 		<>
@@ -94,10 +114,20 @@ function App() {
 							<>
 								<tr>
 									<td>{c.block}</td>
-									<td>{c.name}</td>
 									<td>
-										{c.grade ?? "N/A"}
-										{!!c.grade && "%"}
+										{c.ap ? "AP " : ""}
+										{c.name}
+										{c.honors ? " Honors" : ""}
+									</td>
+									<td onClick={() => changeToInput(index)}>
+										{c.isEditable ? (
+											<input type="number" value={c.grade ?? 0} />
+										) : (
+											<span>
+												{c.grade ?? "N/A"}
+												{!!c.grade && "%"}
+											</span>
+										)}
 									</td>
 								</tr>
 							</>
